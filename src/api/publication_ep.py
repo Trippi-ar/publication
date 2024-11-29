d7809b86d11dd09bab79c97b4dc7c265fd147209
joaquinreyero <joaquinreyero@hotmail.com>
2024-11-29 07:59:20 +0000

    Test fail integration test

--- src/api/publication_ep.py
+++ src/api/publication_ep.py
@@ -42,7 +42,7 @@ def get_publication(
     """
     Get a publication by id
     """
-    return publication_service.get(publication_id)
+    return None
 
 
 @router.get("/", status_code=status.HTTP_200_OK, response_model=List[publication_schema.GetResponse])
